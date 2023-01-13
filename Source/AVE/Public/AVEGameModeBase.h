870987165f0b0720a1624f69e20978a60783e776
Woogl <106436076+Woogl@users.noreply.github.com>
2023-01-13 16:11:21 +0900

    첫 커밋. 아자아자

---
+++ Source/AVE/Public/AVEGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AVEGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AVE_API AAVEGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
