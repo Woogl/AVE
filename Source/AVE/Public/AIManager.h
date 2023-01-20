9f36807cec07a4ee4d503bbb0df33b028bf226e0
Tiphreth <34757938+Tiphreth1@users.noreply.github.com>
2023-01-20 13:56:55 +0900

    일단 주석

--- Source/AVE/Public/AIManager.h
+++ Source/AVE/Public/AIManager.h
@@ -1,31 +1,31 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#pragma once
-
-#include "CoreMinimal.h"
-#include "GameFramework/Pawn.h"
-#include "AIManager.generated.h"
-
-UCLASS()
-class AVE_API AAIManager : public APawn
-{
-	GENERATED_BODY()
-
-public:
-	// Sets default values for this pawn's properties
-	AAIManager();
-
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
-
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-
-	/*UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	class AAIController* MyManager;*/
-};
+//#pragma once
+//
+//#include "CoreMinimal.h"
+//#include "GameFramework/Pawn.h"
+//#include "AIManager.generated.h"
+//
+//UCLASS()
+//class AVE_API AAIManager : public APawn
+//{
+//	GENERATED_BODY()
+//
+//public:
+//	// Sets default values for this pawn's properties
+//	AAIManager();
+//
+//protected:
+//	// Called when the game starts or when spawned
+//	virtual void BeginPlay() override;
+//
+//public:	
+//	// Called every frame
+//	virtual void Tick(float DeltaTime) override;
+//
+//	// Called to bind functionality to input
+//	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+//
+//	/*UPROPERTY(EditAnywhere, BlueprintReadWrite)
+//	class AAIController* MyManager;*/
+//};
